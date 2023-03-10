4f9f2b26a764e300d4fcc9d71d95d8c601d0056b
adamma-da <adam.marton@digitalasset.com>
2023-03-10 16:09:55 +0100

    Added workaround for issue with CR index

--- scripts/chart_publish.sh
+++ scripts/chart_publish.sh
@@ -31,8 +31,11 @@ for chart in $CHARTS_DIR/*; do
         helm package $chart -d $PUBLISH_FOLDER
         cr upload -b https://api.github.com/ -u https://uploads.github.com -c $PUBLISH_BRANCH -r $GITREPO_NAME  -p $PUBLISH_FOLDER --owner $GITREPO_OWNER --skip-existing --token $1
         echo "Chart $chart_name version $chart_version has been pushed to repository $HELMREPO_NAME"
+        helm repo index publish --url https://github.com/adamma-da/test-chart-releaser/releases/download/$chart_name-$chart_version/  --merge docs/index.yaml
+
     fi
 done
 
-cr index --pages-branch $PAGES_BRANCH -b https://api.github.com/ -u https://uploads.github.com -i docs/index.yaml -r $GITREPO_NAME  -p $PUBLISH_FOLDER --owner $GITREPO_OWNER --token $1
+cp publish/index.yaml docs/index.yaml
+# cr index --pages-branch $PAGES_BRANCH -b https://api.github.com/ -u https://uploads.github.com -i docs/index.yaml -r $GITREPO_NAME  -p $PUBLISH_FOLDER --owner $GITREPO_OWNER --token $1
 rm -rf $PUBLISH_FOLDER
