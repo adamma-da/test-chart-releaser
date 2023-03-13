508e33505c2a2afb7d1dd882fbec956a84dc5c61
adamma-da <adam.marton@digitalasset.com>
2023-03-13 16:34:24 +0100

    Publishing crossplane 2.1.4

--- scripts/chart_publish.sh
+++ scripts/chart_publish.sh
@@ -8,9 +8,9 @@ GITREPO_NAME=test-chart-releaser
 GITREPO_OWNER=adamma-da
 PUBLISH_BRANCH=main
 PUBLISH_FOLDER="publish"
-PAGES_BRANCH=main
 
 
+#Set up some workspace for the script
 mkdir $PUBLISH_FOLDER
 helm repo add $HELMREPO_NAME https://$GITREPO_OWNER.github.io/$GITREPO_NAME/
 git config user.email "machine@digitalasset.com"
@@ -36,22 +36,24 @@ for chart in $CHARTS_DIR/*; do
     fi
 done
 
-
+# Overwrite the old index file with the merged index file.
 mv publish/index.yaml docs/index.yaml
 
 # CR index doesnt work for multiple reasons. One being it uses a folder to create an index and it has no file merge capabilities therefore only the files present in the folder would be added to the index.
-#cr index --pr --pages-branch $PAGES_BRANCH -b https://api.github.com/ -u https://uploads.github.com -i docs/index.yaml -r $GITREPO_NAME  -p $PUBLISH_FOLDER --owner $GITREPO_OWNER --token $1
+#cr index --pr --pages-branch $PUBLISH_BRANCH -b https://api.github.com/ -u https://uploads.github.com -i docs/index.yaml -r $GITREPO_NAME  -p $PUBLISH_FOLDER --owner $GITREPO_OWNER --token $1
 
+#We dont need the packaged charts anymore so we remove the whole folder.
 rm -rf $PUBLISH_FOLDER
 
+#Now we need to create a new branch with the changes of the index yaml file and create a PR
 if [[ -z $(git status -s) ]]; then
   echo "No changes to push"
 else
   echo "Pushing changes"
-  git add .
-  git checkout -b "$PUBLISH_BRANCH-index-update" --track
+  git add docs/index.yaml
+  git checkout -b "$PUBLISH_BRANCH-index-update-$CIRCLE_BUILD_NUM" --track
   git commit -m "Updated index.yaml file so that it contains the newly pushed helm charts"
-  git push -f origin "$PUBLISH_BRANCH-index-update"
+  git push -f origin "$PUBLISH_BRANCH-index-update-$CIRCLE_BUILD_NUM"
   current_pr_closed=$(gh pr status --json closed -q '.currentBranch.closed')
   if [[ -z $current_pr_closed ]] || [[ $current_pr_closed == "true" ]] ; then
     echo "Opening new PR."
@@ -63,3 +65,4 @@ fi
 
 helm repo update $HELMREPO_NAME
 helm search repo $HELMREPO_NAME -l  
+echo "Please keep in mind that in order to see your newest release in the helm repo you first have to merge in the MR and then wait up to 2 minutes so that the github page can be rebuilt. Then, you can run helm repo update {reponame} and then helm search {reponame} to see your updated repository."
