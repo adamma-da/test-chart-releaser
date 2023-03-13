da937364b4d443f5259b6d79a59e86e3960d6545
adamma-da <adam.marton@digitalasset.com>
2023-03-13 12:10:07 +0100

    Adding push changes capability

--- scripts/chart_publish.sh
+++ scripts/chart_publish.sh
@@ -3,11 +3,9 @@
 
 # Set up script variables
 CHARTS_DIR=./charts/
-
 HELMREPO_NAME=test-chart-releaser
 GITREPO_NAME=test-chart-releaser
 GITREPO_OWNER=adamma-da
-
 PUBLISH_BRANCH=main
 PUBLISH_FOLDER="publish"
 PAGES_BRANCH=main
@@ -15,7 +13,6 @@ PAGES_BRANCH=main
 
 mkdir $PUBLISH_FOLDER
 helm repo add $HELMREPO_NAME https://$GITREPO_OWNER.github.io/$GITREPO_NAME/
-
 git config user.email "machine@digitalasset.com"
 git config user.name "machine-da"
 
@@ -34,14 +31,31 @@ for chart in $CHARTS_DIR/*; do
         helm package $chart -d $PUBLISH_FOLDER
         cr upload -b https://api.github.com/ -u https://uploads.github.com --skip-existing -c $PUBLISH_BRANCH -r $GITREPO_NAME  -p $PUBLISH_FOLDER --owner $GITREPO_OWNER --token $1
         echo "Chart $chart_name version $chart_version has been pushed to repository $HELMREPO_NAME"
-        #helm repo index publish --url https://github.com/adamma-da/test-chart-releaser/releases/download/$chart_name-$chart_version/  --merge docs/index.yaml
+        helm repo index publish --url https://github.com/adamma-da/test-chart-releaser/releases/download/$chart_name-$chart_version/  --merge docs/index.yaml
 
     fi
 done
 
-#mv publish/index.yaml docs/index.yaml
-# this doesnt work due to a bug in CR.
-#
-cr index --pr --pages-branch $PAGES_BRANCH -b https://api.github.com/ -u https://uploads.github.com -i docs/index.yaml -r $GITREPO_NAME  -p $PUBLISH_FOLDER --owner $GITREPO_OWNER --token $1
-#
+mv publish/index.yaml docs/index.yaml
+
+# CR index doesnt work for multiple reasons. One being it uses a folder to create an index and it has no file merge capabilities therefore only the files present in the folder would be added to the index.
+#cr index --pr --pages-branch $PAGES_BRANCH -b https://api.github.com/ -u https://uploads.github.com -i docs/index.yaml -r $GITREPO_NAME  -p $PUBLISH_FOLDER --owner $GITREPO_OWNER --token $1
+
 rm -rf $PUBLISH_FOLDER
+
+if [[ -z $(git status -s) ]]; then
+  echo "No changes to push"
+else
+  echo "Pushing changes"
+  git add .
+  git checkout -b "$branch_name" --track
+  git commit -m "Updated index.yaml file so that it contains the newly pushed helm charts"
+  git push -f origin "$branch_name"
+  current_pr_closed=$(gh jq --run "gh pr status --json closed -q '.currentBranch.closed'")
+  if [[ -z $current_pr_closed ]] || [[ $current_pr_closed == "true" ]] ; then
+    echo "Opening new PR."
+    gh pr create --fill
+  else
+    echo "PR already exists."
+  fi
+fi
